06a1e3c6f6046dd41bd220f38f2946286a2f2ae1
yashi <yashi.songarra@bhavitechnologies.com>
2020-04-28 19:03:07 +0530

    structure addition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"list",
+    component:ListComponent
+  },
+  {
+    path :"detail/:id",
+    component:DetailComponent
+  },
+  {
+    path: "",
+    redirectTo:"list",
+    pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
