9b5823f7b7dfcd5b52c7a1bbc0bfa53369ab4d17
Meet Shah <meetdshah007@gmail.com>
2020-05-03 18:33:39 +0530

    Website beautification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pokedex';
+export class AppComponent implements OnInit{
+  displayFooter = false;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.displayFooter = true;
+    }, 700);
+  }
 }
